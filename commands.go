49212d6eddea4fc91111e23a359f5ae75b2a9920
anders-14 <anders.fimreite@gmail.com>
2020-08-09 15:13:48 +0200

    Readding rps and commands command

--- commands.go
+++ commands.go
@@ -11,6 +11,12 @@ type Command struct {
 
 var Commands = [...]Command{
 	{
+		name: "commands",
+		exec: func(c *Client, pc *ParsedCommand) {
+			c.SendMessage("Go to bot-anders14-commands.vercel.app for a list of all the commands and what they do")
+		},
+	},
+	{
 		name: "ping",
 		exec: func(c *Client, pc *ParsedCommand) {
 			msg := fmt.Sprintf("Pong, @%s", pc.user.displayname)
@@ -36,6 +42,20 @@ var Commands = [...]Command{
 		exec: func(c *Client, pc *ParsedCommand) {
 			trivia := FetchTodayTrivia().trivia
 			c.SendMessage(trivia)
+		},
+	},
+	{
+		name: "rps",
+		exec: func(c *Client, pc *ParsedCommand) {
+			userMove := pc.args[0]
+			outcome := PlayRPS(userMove)
+			msg := ""
+			if outcome != "" {
+				msg = fmt.Sprintf("%s @%s", outcome, pc.user.displayname)
+			} else {
+				msg = fmt.Sprintf("Not a valid move @%s", pc.user.displayname)
+			}
+			c.SendMessage(msg)
 		},
 	},
 }
